17aa0f70db57ab2226fc3daa082ce9ecb2caad4c
Artem Ulko <ulkoart@gmail.com>
2020-08-10 10:50:14 +0300

    fetch weather by coord

--- Sunny/Models/NetworkWeatherManager.swift
+++ Sunny/Models/NetworkWeatherManager.swift
@@ -7,23 +7,55 @@
 //
 
 import UIKit
+import CoreLocation
 
-struct NetworkWeatherManager {
-    func fetchCurrentWeather(forCity city: String, completionHandler: @escaping (CurrentWeather) -> Void) {
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)"
-        guard let url = URL(string: urlString) else { return }
-        let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: url) {data, response, error in
-            if let data = data {
-                if let currentWeather = self.parseJSON(withData: data) {
-                completionHandler(currentWeather)
-                }
-            }
-        }
-        task.resume()
+protocol NetworkWeatherManagerDelegate: class {
+    func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather)
+}
+
+class NetworkWeatherManager {
+    
+    enum RequestType {
+        case cityName(city: String)
+        case coordinate(latitude: CLLocationDegrees, longitude: CLLocationDegrees)
     }
     
-    func parseJSON(withData data: Data) -> CurrentWeather? {
+    var onComplition: ((CurrentWeather) -> Void)?
+    weak var delegate: NetworkWeatherManagerDelegate?
+    
+    func fetchCurrentWeather(forRequestType requestType: RequestType) {
+        var urlString = ""
+        
+        switch requestType {
+        case .cityName(let city):
+            urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)&units=metric"
+        case .coordinate(let latitude, let longitude):
+            urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric"
+
+        }
+        
+        print(urlString)
+        
+        perfomeRequest(withURLString: urlString)
+    }   
+   
+    fileprivate func perfomeRequest(withURLString urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+               let session = URLSession(configuration: .default)
+               let task = session.dataTask(with: url) {data, response, error in
+                   if let data = data {
+                       if let currentWeather = self.parseJSON(withData: data) {
+                           //                    self.delegate?.updateInterface(self, with: currentWeather)
+                           self.onComplition?(currentWeather)
+                       }
+                   }
+               }
+               task.resume()
+    }
+    
+    
+    
+    fileprivate func parseJSON(withData data: Data) -> CurrentWeather? {
         let decoder = JSONDecoder()
         
         do {
--- Sunny/View Controllers/ViewController.swift
+++ Sunny/View Controllers/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
     
@@ -16,21 +17,76 @@ class ViewController: UIViewController {
     @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
     
     var networkWeatherManager = NetworkWeatherManager()
+    lazy var locationManager: CLLocationManager = {
+        let lm = CLLocationManager()
+        lm.delegate = self
+        lm.desiredAccuracy = kCLLocationAccuracyKilometer
+        lm.requestWhenInUseAuthorization()
+        return lm
+    }()
     
     
     @IBAction func searchPressed(_ sender: UIButton) {
         self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) {
-            cityName in
-            self.networkWeatherManager.fetchCurrentWeather(forCity: cityName) { currentWeather in
-                print(currentWeather)
+            [unowned self] city in
+            self.networkWeatherManager.fetchCurrentWeather(forRequestType: .cityName(city: city))
+            
+            
+            self.networkWeatherManager.onComplition = {[weak self] currentWeather in
+                guard let self = self else { return }
+                self.updateInterface(with: currentWeather)
             }
+            
+            
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        networkWeatherManager.fetchCurrentWeather(forCity: "Krasnodar")
+        // networkWeatherManager.delegate = self
+        self.networkWeatherManager.onComplition = {[weak self] currentWeather in
+            guard let self = self else { return }
+            self.updateInterface(with: currentWeather)
+        }
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func updateInterface(with weather: CurrentWeather) {
+        
+        DispatchQueue.main.async {
+            self.cityLabel.text = weather.cityName
+            self.temperatureLabel.text = weather.temperatureString
+            self.feelsLikeTemperatureLabel.text = weather.feelsLikeTemperatureString
+            self.weatherIconImageView.image = UIImage(systemName: weather.systemIconNameString)
+        }
+        
+        
     }
 }
 
+// MARK: - CLLocationManagerDelegate
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+            return
+        }
+        let latitude = location.coordinate.latitude
+        let longitude = location.coordinate.longitude
+        
+        networkWeatherManager.fetchCurrentWeather(forRequestType: .coordinate(latitude: latitude, longitude: longitude))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+}
+
+//extension ViewController: NetworkWeatherManagerDelegate {
+//    func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather) {
+//        print(currentWeather.cityName)
+//    }
+//}
